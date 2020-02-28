ddb8a6e88f1790ac219a536d993f5b502eeea057
Emeka Ugwuanyi <emylincon@gmail.com>
2020-02-28 14:02:09 +0000

    first commit

--- email_me.py
+++ email_me.py
@@ -1,18 +1,17 @@
 import getpass as gp
 import smtplib
 import os
-
-email_address = "spicetala@gmail.com"
+import config
 
 
 def send_email(msg, password, s_email):
     try:
         server = smtplib.SMTP_SSL('smtp.gmail.com')
         server.ehlo()
-        server.login(email_address, password)
+        server.login(config.email_address, password)
         subject = "Tutorial Example"
-        _message = 'Subject: {}\n\n{}\n\n SENT BY RIHANNA \n\n'.format(subject, msg)
-        server.sendmail(email_address, s_email, _message)
+        _message = 'Subject: {}\n\n{}\n\n SENT BY BOT \n\n'.format(subject, msg)
+        server.sendmail(config.email_address, s_email, _message)
         server.quit()
         print("Email sent!")
     except Exception as e:
@@ -20,11 +19,10 @@ def send_email(msg, password, s_email):
 
 
 def main():
-    pass_ = gp.getpass('Enter Email password: ')
+    #pass_ = gp.getpass('Enter Email password: ')
     sender_email = input('Enter email address to send to: ')
     message = input('Enter message: ')
-    #x = input('password: ')
-    send_email(msg=message, password=pass_, s_email=sender_email)
+    send_email(msg=message, password=config.password, s_email=sender_email)
 
 
 if __name__ == '__main__':
